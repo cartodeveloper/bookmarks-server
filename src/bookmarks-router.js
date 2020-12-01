c2c3ab1294dfb30de8d0c18ba0e3b4fb16f534d8
María Torrente <caritotorrente@gmail.com>
2020-12-01 10:33:55 -0500

    creating the POST/bookmarks router

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -5,8 +5,29 @@ const logger = require("./logger");
 const bookmarksRouter = express.Router();
 const bodyParser = express.json();
 
-bookmarksRouter.route("/bookmarks").get((req, res) => {
-  res.json(bookmarks);
-});
+bookmarksRouter
+  .route("/bookmarks")
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    for (const field of ["title", "url", "rating"]) {
+      if (!req.body[field]) {
+        logger.error(`${field} is required`);
+        return res.status(400).send(`${field} is required`);
+      }
+    }
 
+    const { title, url, description, rating } = req.body;
+
+    const bookmark = { id: uuid(), title, url, description, rating };
+
+    bookmarks.push(bookmark);
+
+    logger.info(`Bookmark with id ${bookmark.id} created`);
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+      .json(bookmark);
+  });
 module.exports = bookmarksRouter;
