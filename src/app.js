3a05ba26353be9e304a9a982f1fb3d7181084f62
María Torrente <caritotorrente@gmail.com>
2020-12-01 10:13:32 -0500

    modularizing and setting the proper validation for the API_KEY Bearer Token

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const validateBearerToken = require("./validateBearerToken");
 const errorHandler = require("./error-handler");
 
 const app = express();
@@ -12,6 +13,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
+app.use(validateBearerToken);
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
