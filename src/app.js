4acf177b8000093f2990b9ee937dbce6cddd81ae
María Torrente <caritotorrente@gmail.com>
2020-12-01 10:30:24 -0500

    Creating GET/bookmarks router and modularizing/importing into app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const validateBearerToken = require("./validateBearerToken");
 const errorHandler = require("./error-handler");
+const bookmarksRouter = require("./bookmarks-router");
 
 const app = express();
 
@@ -14,9 +15,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(validateBearerToken);
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use(bookmarksRouter);
 
 app.use(cors());
 app.use(errorHandler);
--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const uuid = require("uuid/v4");
 const logger = require("./logger");
+
+const bookmarksRouter = express.Router();
+const bodyParser = express.json();
+
+bookmarksRouter.route("/bookmarks").get((req, res) => {
+  res.json(bookmarks);
+});
+
+module.exports = bookmarksRouter;
